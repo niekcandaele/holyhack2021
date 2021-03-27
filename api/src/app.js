7319b50b0491c4df26123b72543aa020113fed4d
jarorobbe <jarorobbe@hotmail.com>
2021-03-27 09:11:24 +0100

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const router = require('./routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/', router);
 
