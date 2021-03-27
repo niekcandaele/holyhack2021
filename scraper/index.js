0a968332be04c81153394029c5ea688e7eace402
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2021-03-27 01:51:16 +0100

    rate limiting

--- scraper/index.js
+++ scraper/index.js
@@ -1,9 +1,69 @@
 require('dotenv').config()
-const { getAllMovies } = require('./movies')
-const { getAllShows } = require('./shows')
+const { wait, store, TYPES, redis } = require('./lib');
+const baseUrlMovieDB = `https://api.themoviedb.org/3`;
+const axios = require('axios').default
 
 // Get this many pages of objects, or until empty responses
 const iterations = 500
 
-getAllMovies(iterations)
-getAllShows(iterations)
+getAll(TYPES.MOVIE)
+getAll(TYPES.SHOW)
+
+async function getAll(type) {
+    let currentCursor = 1
+
+    while (currentCursor < iterations) {
+        console.log(`Getting ${type} page ${currentCursor}`);
+        const res = await get(currentCursor, type)
+        currentCursor++
+
+        if (!res) {
+            // No data returned, skipping this page
+            continue
+        }
+
+        // Empty response, end the loop
+        if (!res.data.results.length) {
+            currentCursor = iterations
+        }
+
+        await store(res.data.results, type)
+        await wait()
+        await redis.set(`${type}:${currentCursor}`, true);
+
+    }
+}
+
+
+async function get(page, type) {
+
+    const exists = await redis.exists(`${type}:${page}`)
+    if (exists) {
+        console.log(`${type}:${page} already exists, skipping.`);
+        return
+    }
+
+
+    switch (type) {
+        case TYPES.MOVIE:
+            return axios.get(`${baseUrlMovieDB}/discover/movie`, {
+                params: {
+                    sort_by: 'popularity.desc',
+                    page,
+                    api_key: process.env.TMDB_API_KEY,
+                }
+            })
+        case TYPES.SHOW:
+            return axios.get(`${baseUrlMovieDB}/discover/tv`, {
+                params: {
+                    sort_by: 'popularity.desc',
+                    page,
+                    api_key: process.env.TMDB_API_KEY,
+                }
+            })
+        default:
+            throw new Error('invalid type')
+    }
+
+
+}
